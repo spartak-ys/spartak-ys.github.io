b7c15e4ea10c2b0b44beca6119ba1f646a18830b
spartak-ys <Spartakhayrapetyan161215@gmail.com>
2025-05-31 11:37:52 +0400

    Add files via upload

---
+++ RandomNumberGame/script.js
@@ -0,0 +1,16 @@
+var input = document.getElementById("input");
+var button = document.getElementById("button");
+var patasxan = document.getElementById("patasxan");
+
+let rn = Math.floor(Math.random()*10);
+console.log(rn);
+
+button.addEventListener("click", function(){
+    let a;
+    if (parseInt(input.value)==rn) {
+    a="Դու ճիշտ ես։";
+} else{
+    a="Դու սխալ ես։";
+}
+patasxan.innerText = a;
+});
