fab2c374bfd3bbdf76706a2c7a14a9d9ba75582e
spartak-ys <Spartakhayrapetyan161215@gmail.com>
2026-02-07 23:10:33 +0400

    Add files via upload

---
+++ matemdproc/script.js
@@ -0,0 +1,90 @@
+function patasxan1() {
+    const patasxan1 = +document.getElementById("input1").value
+
+    if (patasxan1 === 112) {
+        document.getElementById("result1").innerText = "Ճիշտ է";
+    } else {
+        document.getElementById("result1").innerText = "Սխալ է";
+    }
+}
+function patasxan2() {
+    const patasxan2 = +document.getElementById("input2").value
+
+    if (patasxan2 === 36) {
+        document.getElementById("result2").innerText = "Ճիշտ է";
+    } else {
+        document.getElementById("result2").innerText = "Սխալ է";
+    }
+}
+function patasxan3() {
+    const patasxan3 = +document.getElementById("input3").value
+
+    if (patasxan3 === 140) {
+        document.getElementById("result3").innerText = "Ճիշտ է";
+    } else {
+        document.getElementById("result3").innerText = "Սխալ է";
+    }
+}
+function patasxan4() {
+    const patasxan4 = +document.getElementById("input4").value
+
+    if (patasxan4 === 132) {
+        document.getElementById("result4").innerText = "Ճիշտ է";
+    } else {
+        document.getElementById("result4").innerText = "Սխալ է";
+    }
+}
+function patasxan5() {
+    const patasxan5 = +document.getElementById("input5").value
+
+    if (patasxan5 === 210) {
+        document.getElementById("result5").innerText = "Ճիշտ է";
+    } else {
+        document.getElementById("result5").innerText = "Սխալ է";
+    }
+}        
+function patasxan6() {
+    const patasxan6 = +document.getElementById("input6").value
+
+    if (patasxan6 === 135) {
+        document.getElementById("result6").innerText = "Ճիշտ է";
+    } else {
+        document.getElementById("result6").innerText = "Սխալ է";
+    }
+}
+function patasxan7() {
+    const patasxan7 = +document.getElementById("input7").value
+
+    if (patasxan7 === 48) {
+        document.getElementById("result7").innerText = "Ճիշտ է";
+    } else {
+        document.getElementById("result7").innerText = "Սխալ է";
+    }
+} 
+function patasxan8() {
+    const patasxan8 = +document.getElementById("input8").value
+
+    if (patasxan8 === 308) {
+        document.getElementById("result8").innerText = "Ճիշտ է";
+    } else {
+        document.getElementById("result8").innerText = "Սխալ է";
+    }
+} 
+function patasxan9() {
+    const patasxan9 = +document.getElementById("input9").value
+
+    if (patasxan9 === 224) {
+        document.getElementById("result9").innerText = "Ճիշտ է";
+    } else {
+        document.getElementById("result9").innerText = "Սխալ է";
+    }
+}
+function patasxan10() {
+    const patasxan10 = +document.getElementById("input10").value
+
+    if (patasxan10 === 144) {
+        document.getElementById("result10").innerText = "Ճիշտ է";
+    } else {
+        document.getElementById("result10").innerText = "Սխալ է";
+    }
+}          
