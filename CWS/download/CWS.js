25233fbaedadf071c480395a178f02691a37d5dc
spartak-ys <Spartakhayrapetyan161215@gmail.com>
2026-04-09 16:38:25 +0400

    Add files via upload

---
+++ CWS/download/CWS.js
@@ -0,0 +1,59 @@
+const COLORS = {
+    0: "white",
+    1: "black",
+    2: "red",
+    3: "green",
+    4: "blue",
+    5: "yellow",
+    6: "purple",
+    7: "cyan",
+    8: "grey",
+    9: "orange",
+    p: "pink"
+};
+
+const CWS = {
+    // Create a sprite object
+    createSprite: (img, size = 20, x = 0, y = 0) => {
+        return { img, size, x, y };
+    },
+
+    // Draw a single sprite
+    drawSprite: (sprite, canvas) => {
+        let ctx = canvas.getContext("2d");
+
+        for (let y = 0; y < sprite.img.length; y++) {
+            for (let x = 0; x < sprite.img[y].length; x++) {
+                let value = sprite.img[y][x];
+                ctx.fillStyle = COLORS[value] || value;
+                ctx.fillRect(
+                    x * sprite.size + sprite.x,
+                    y * sprite.size + sprite.y,
+                    sprite.size,
+                    sprite.size
+                );
+            }
+        }
+    },
+
+    // Draw multiple sprites
+    drawSprites: (sprites, canvas) => {
+        sprites.forEach(sprite => {
+            CWS.drawSprite(sprite, canvas);
+        });
+    },
+
+    // Move a sprite by dx, dy
+    moveSprite: (sprite, dx, dy) => {
+        sprite.x += dx;
+        sprite.y += dy;
+    },
+
+    // Clear the canvas
+    clear: (canvas) => {
+        let ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+};
+
+export default CWS;
