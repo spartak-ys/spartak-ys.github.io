de9eedb8d21528a0d58791537000d2dd093b0814
spartak-ys <Spartakhayrapetyan161215@gmail.com>
2026-04-10 18:42:52 +0400

    Add files via upload

---
+++ CWS/download/CWS.1.0.2.js
@@ -0,0 +1,112 @@
+const COLORS = {
+    0: "white",
+    1: "black",
+    2: "red",
+    3: "green",
+    4: "blue",
+    5: "yellow",
+    6: "purple",
+    7: "cyan",
+    8: "grey",
+    9: "rgb(255, 83, 0)",
+};
+
+const CWS = {
+
+    createSprite: (img, size = 20, x = 0, y = 0) => {
+        let height = img.length*size;
+        let width = img[0].length*size;
+        return { img, size, x, y, width, height };
+    },
+
+
+    drawSprite: (sprite, canvas) => {
+        let ctx = canvas.getContext("2d");
+
+        for (let y = 0; y < sprite.img.length; y++) {
+            for (let x = 0; x < sprite.img[y].length; x++) {
+                let value = sprite.img[y][x];
+                if(COLORS.hasOwnProperty(value)){
+                    ctx.fillStyle = COLORS[value];
+                    ctx.fillRect(
+                        (x * sprite.size) + sprite.x,
+                        (y * sprite.size) +  sprite.y,
+                        sprite.size,
+                        sprite.size
+                    );
+                }
+        }
+        }
+    },
+
+    drawSprites: (sprites, canvas) => {
+        sprites.forEach(sprite => {
+            CWS.drawSprite(sprite, canvas);
+        });
+    },
+
+    moveSprite: (sprite, dx, dy) => {
+        sprite.x += dx;
+        sprite.y += dy;
+    },
+
+    clear: (canvas) => {
+        let ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    },
+
+    isCollision: (a, b) => {
+    return (
+        a.x < b.x + b.width &&
+        a.x + a.width > b.x &&
+        a.y < b.y + b.height &&
+        a.y + a.height > b.y
+    );
+    },
+
+    isInCanvas: (sprite, canvas) => {
+        return (
+            sprite.x >= 0 &&
+            sprite.y >= 0 &&
+            sprite.x + sprite.width <= canvas.width &&
+            sprite.y + sprite.height <= canvas.height
+        );
+    },
+    setPosition: (sprite, x, y) => {
+        sprite.x = x;
+        sprite.y = y;
+    },
+    centerSprite: (sprite, canvas) => {
+        sprite.x = canvas.width / 2 - sprite.width / 2;
+        sprite.y = canvas.height / 2 - sprite.height / 2;
+    },
+
+    centerSpriteByX:(sprite, canvas)=>{
+        sprite.x = canvas.width / 2 - sprite.width / 2;
+    },
+
+    centerSpriteByY: (sprite, canvas)=>{
+        sprite.y = canvas.height / 2 - sprite.height / 2;
+    },
+
+    cloneSprite: (sprite, x, y, size) => {
+        return {
+            img: sprite.img,
+            size,
+            x,
+            y,
+            width: img[0].length*size,
+            height: img.length*size
+        };
+    },
+
+    followSprite: (enemy, player, speed = 1) => {
+        if (enemy.x < player.x) enemy.x += speed;
+        if (enemy.x > player.x) enemy.x -= speed;
+
+        if (enemy.y < player.y) enemy.y += speed;
+        if (enemy.y > player.y) enemy.y -= speed;
+    },
+};
+
+export default CWS;
---
+++ CWS/index.js
@@ -0,0 +1,64 @@
+import CWS from "./download/CWS.1.0.3.js";
+
+let heartArt = document.getElementById("heartArt");
+let downlode = document.getElementById("downlode");
+let mainDownlode = document.getElementById("mainDownlode");
+let main = document.getElementById("main");
+let mainMain = document.getElementById("mainMain");
+let SSHA = document.getElementById("SSHA");
+let canvas = document.getElementById("canvas");
+let tryBtn = document.getElementById("try")
+let mainTry = document.getElementById("mainTry")
+
+window.runCode = function () {
+    let code = document.getElementById("code").value
+    eval(code)
+}
+
+runCode()
+
+heartArt.width = 50
+heartArt.height = 50
+SSHA.width = SSHA.parentElement.clientWidth;
+SSHA.height = SSHA.parentElement.clientHeight;
+
+let heart = CWS.createSprite([
+    ["",2 , "", 2, ""],
+    [2, 2, 2, 2, 2],
+    [2, 2, 2, 2, 2],
+    ["", 2, 2, 2, ""],
+    ["", "", 2, "",""]
+], 10)
+    CWS.drawSprite(heart, heartArt)
+
+let SSH = CWS.createSprite(CWS.spriteGalery.Rocket, 5, (SSHA.width-65)/2, (SSHA.height-120))
+
+
+function  gameLoop() {
+    CWS.clear(SSHA)
+    CWS.drawSprite(SSH, SSHA)
+    CWS.moveSprite(SSH, 0, -1)
+    requestAnimationFrame(gameLoop)
+    if (SSH.y + 120 < 0) { 
+        SSH.y = SSHA.height; 
+    }
+}
+gameLoop()
+
+downlode.addEventListener("click", ()=>{
+    mainDownlode.style.display = "flex";
+    mainMain.style.display = "none";
+    mainTry.style.display = "none";
+})
+
+main.addEventListener("click", ()=>{
+    mainDownlode.style.display = "none";
+    mainMain.style.display = "flex";
+    mainTry.style.display = "none";
+})
+
+tryBtn.addEventListener("click", ()=>{
+    mainDownlode.style.display = "none";
+    mainMain.style.display = "none";
+    mainTry.style.display = "flex";
+})
